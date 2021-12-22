221b67ec8dba239fc90d4fa2c87e1fcdf1a3f3ed
Martin Gejke <martin.gejke@consultant.volvo.com>
2021-12-22 14:20:21 +0100

    Convert to fastpi & uvicorn

--- player.py
+++ player.py
@@ -1,13 +1,21 @@
+import random
+from typing import List
+
 from models import Action, ActionRequest, Mark
 
 # Player controll logic
-def get_actions(state: ActionRequest):
-  for y in range(0, 3):
-    for x in range(0, 3):
-        if state.state.board[y][x] == Mark.NONE:
-            return [Action(x=x, y=y)]
-
-  return []
+def get_actions(action_request: ActionRequest) -> List[Action]:
+    possible = []
+    for y, row in enumerate(action_request.state.board):
+      for x, mark in enumerate(row):
+            if mark == Mark.NONE:
+                possible.append(Action(x=x, y=y))
+            
+    if possible:
+        return [random.choice(possible)]
+    else:
+        return []
+  
 
 def get_player_info():
   return {
